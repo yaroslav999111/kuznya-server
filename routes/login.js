c6945fbc3f13a3a883f146f7457a0d8cb2878854
YAro <carlend94@ukr.net>
2018-11-27 13:54:45 +0200

    added server email

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var passport = require('passport');
 var User = require('../models/user');
 var bcrypt = require('bcryptjs');
+var nodemailer = require('nodemailer');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('register works fine');
@@ -49,5 +50,33 @@ router.post('/addRating', function(req, res, next) {
         }
     });
 });
+
+
+router.post('/sendemail', function(req, res, next) {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'proamazons@gmail.com',
+            pass: 'proamz123'
+        }
+    });
+
+    const mailOptions = {
+        from: req.body.email, // sender address
+        to: 'proamazons@gmail.com', // list of receivers
+        subject: 'Subject of your email', // Subject line
+        html: req.body.text + req.body.phone// plain text body
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+        if(err)
+            console.log(err);
+        else
+            console.log(info);
+    });
+
+});
+
+
 
 module.exports = router;
